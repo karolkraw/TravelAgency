2fa88b8a06e6cee859dc015597fb0c51f31bcf22
Karol <karolkrawczyk8650@gmail.com>
2023-02-27 20:04:19 +0100

    add new tables required from spring security

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,17 +1,28 @@
+
 INSERT INTO destination (destination) VALUES ('Paris'), ('New York'), ('Tokyo');
 
-INSERT INTO tour_guide (first_name, last_name) VALUES ('John', 'Doe'), ('Jane', 'Smith');
 
-INSERT INTO trip (price, departure_date, return_date, destination_id, tour_guide_id) VALUES
+INSERT INTO guide (first_name, last_name) VALUES ('John', 'Doe'), ('Jane', 'Smith');
+
+INSERT INTO trip (price, departure_date, return_date, destination_id, guide_id) VALUES
     (1000.00, '2023-03-01', '2023-03-07', 1, 1),
     (1500.00, '2023-04-01', '2023-04-07', 2, 2),
-    (2000.00, '2023-05-01', '2023-05-07', 3, 1);
+    (3000.00, '2023-08-01', '2023-05-07', 3, 1),
+    (2000.00, '2023-05-01', '2023-05-07', 1, 1);
 
-INSERT INTO client (first_name, last_name, passport_number) VALUES ('Alice', 'Jones', '123456'), ('Bob', 'Smith', '789012');
 
-INSERT INTO trip_client (trip_id, client_id) VALUES
+INSERT INTO app_user (first_name, last_name, passport_number, email, password, app_user_role, locked, enabled)
+VALUES ('John', 'Doe', '123456', 'john.doe@example.com', 'password123', 'USER', false, true),
+       ('Jane', 'Doe', '654321', 'jane.doe@example.com', 'password456', 'ADMIN', false, true),
+       ('Bob', 'Smith', '987654', 'bob.smith@example.com', 'password789', 'USER', true, true);
+
+INSERT INTO app_user_trips (trip_id, app_user_id) VALUES
     (1, 1),
     (1, 2),
     (2, 2),
     (3, 1),
-    (3, 2);
+    (3, 2),
+    (4, 3);
+
+
+
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,8 @@
-DROP TABLE IF EXISTS trip_client;
+DROP TABLE IF EXISTS app_user_trips;
 DROP TABLE IF EXISTS trip;
-DROP TABLE IF EXISTS tour_guide;
-DROP TABLE IF EXISTS client;
+DROP TABLE IF EXISTS guide;
+DROP TABLE IF EXISTS confirmation_token;
+DROP TABLE IF EXISTS app_user;
 DROP TABLE IF EXISTS destination;
 
 CREATE TABLE destination (
@@ -9,7 +10,7 @@ CREATE TABLE destination (
     destination VARCHAR (50) NOT NULL
 );
 
-CREATE TABLE tour_guide (
+CREATE TABLE guide (
      id SERIAL PRIMARY KEY,
      first_name VARCHAR (50) NOT NULL,
      last_name VARCHAR (50) NOT NULL
@@ -21,26 +22,45 @@ CREATE TABLE trip (
     departure_date DATE,
     return_date DATE,
     destination_id BIGINT,
-    tour_guide_id BIGINT,
+    guide_id BIGINT,
     CONSTRAINT fk_destination FOREIGN KEY (destination_id)
         REFERENCES destination (id),
-    CONSTRAINT fk_tour_guide FOREIGN KEY (tour_guide_id)
-        REFERENCES tour_guide (id)
+    CONSTRAINT fk_guide FOREIGN KEY (guide_id)
+        REFERENCES guide (id)
 );
 
-CREATE TABLE client (
+CREATE TABLE app_user (
     id SERIAL PRIMARY KEY,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
-    passport_number VARCHAR(50) NOT NULL
+    passport_number VARCHAR(50) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    app_user_role VARCHAR(255) NOT NULL,
+    locked BOOLEAN NOT NULL DEFAULT false,
+    enabled BOOLEAN NOT NULL DEFAULT false
 );
 
-CREATE TABLE trip_client (
+CREATE TABLE confirmation_token
+(
+    id           SERIAL PRIMARY KEY,
+    token        VARCHAR(255) NOT NULL,
+    created_at   TIMESTAMP    NOT NULL,
+    expires_at   TIMESTAMP    NOT NULL,
+    confirmed_at TIMESTAMP,
+    app_user_id  BIGINT       NOT NULL,
+    CONSTRAINT fk_app_user FOREIGN KEY (app_user_id)
+        REFERENCES app_user (id)
+);
+
+CREATE TABLE app_user_trips (
     trip_id BIGINT,
-    client_id BIGINT,
-    CONSTRAINT pk_trip_client PRIMARY KEY (trip_id, client_id),
+    app_user_id BIGINT,
+    CONSTRAINT pk_trip_app_user PRIMARY KEY (trip_id, app_user_id),
     CONSTRAINT fk_trip FOREIGN KEY (trip_id)
         REFERENCES trip(id),
-    CONSTRAINT fk_client FOREIGN KEY (client_id)
-        REFERENCES client(id)
+    CONSTRAINT fk_app_user FOREIGN KEY (app_user_id)
+        REFERENCES app_user(id)
 );
+
+
