893ddac55fb8e952cb3995e8b0c6a63d9a43a173
Karol <karolkrawczyk8650@gmail.com>
2023-02-27 20:04:48 +0100

    delete

--- src/main/java/com/example/travelagency/repository/ClientRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.travelagency.repository;
-
-import com.example.travelagency.model.Client;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClientRepository extends JpaRepository<Client, Long> {
-}
--- src/main/java/com/example/travelagency/repository/TourGuideRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.travelagency.repository;
-
-import com.example.travelagency.model.TourGuide;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TourGuideRepository extends JpaRepository<TourGuide, Long> {
-}
